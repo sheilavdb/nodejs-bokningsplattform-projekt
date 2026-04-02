14f99f75ffcadb4b039305a0d5f3e98007e20403
sheilavdb <sheilavdb@gmail.com>
2026-04-02 08:57:27 +0200

    find bug step 1 - basic server

--- server.js
+++ server.js
@@ -1,15 +1,61 @@
-import express from "express";
-
-console.log("🔥 FILE STARTED");
+//import 'dotenv/config';
+import express from 'express';
+import http from "http";
+//import { Server } from "socket.io";
+//import logger from "./src/utils/logger.js"
+//import morgan from 'morgan';
+//import connectDB from "./src/config/db.js";
+//import authRoutes from "./src/routes/authRoutes.js"
+//import roomRoutes from './src/routes/roomRoutes.js'
+//import bookingRoutes from './src/routes/bookingRoutes.js'
 
 const app = express();
+const server = http.createServer(app)
+//const io = new Server(server);
 
+//app.set("io", io);
+
+/* Middleware
+app.use(express.json());
+app.use(morgan('dev'));*/
+
+// Serva HTML filer
+//app.use(express.static('public'));
+
+/* Anslut till databas MONGODB
+try{
+  await connectDB();
+  console.log("MongoDB connected");
+} catch (error) {
+  console.log("MongoDB failed", error)
+}*/
+
+/* Socket connection
+io.on('connection', (socket) => {
+  logger.info(`En användare anslöt:, ${socket.id}`);
+  
+  socket.on('disconnect', () => {
+    logger.info(`Användare kopplade bort: ${socket.id}`);
+  });
+}); */
+
+// Routes
+//app.use('/auth', authRoutes);
+//app.use('/rooms', roomRoutes);
+//app.use('/bookings', bookingRoutes)
+
+//REMOVE AFTER TEST!!!
 app.get("/", (req, res) => {
   res.send("Server works!");
 });
 
+/* Api route
+app.get('/api', (req, res) => {
+  res.json({ message: 'Bokningsplattform API' });
+});*/
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("🔥 SERVER RUNNING ON", PORT);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server körs på http://localhost:${PORT}`);
 });
