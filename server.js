1fa40f6f8c371e3caab17fdd170d45ac07a925ad
sheilavdb <sheilavdb@gmail.com>
2026-04-03 10:49:04 +0200

    find bug step 7 - add bookings and socket io

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
 import http from "http";
-//import { Server } from "socket.io";
-//import logger from "./src/utils/logger.js"
+import { Server } from "socket.io";
+import logger from "./src/utils/logger.js"
 import morgan from 'morgan';
 import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routes/authRoutes.js"
 import roomRoutes from './src/routes/roomRoutes.js'
-//import bookingRoutes from './src/routes/bookingRoutes.js'
+import bookingRoutes from './src/routes/bookingRoutes.js'
 
 const app = express();
 const server = http.createServer(app)
-//const io = new Server(server);
+const io = new Server(server);
 
-//app.set("io", io);
+app.set("io", io);
 
 // Middleware
 app.use(express.json());
@@ -22,29 +22,29 @@ app.use(morgan('dev'));
 // Serva HTML filer
 app.use(express.static('public'));
 
-/* Socket connection
+// Socket connection
 io.on('connection', (socket) => {
   logger.info(`En användare anslöt:, ${socket.id}`);
   
   socket.on('disconnect', () => {
     logger.info(`Användare kopplade bort: ${socket.id}`);
   });
-}); */
+}); 
 
 // Routes
 app.use('/auth', authRoutes);
 app.use('/rooms', roomRoutes);
-//app.use('/bookings', bookingRoutes)
+app.use('/bookings', bookingRoutes)
 
 //REMOVE AFTER TEST!!!
 app.get("/", (req, res) => {
   res.send("Server works!");
 });
 
-/* Api route
+// Api route
 app.get('/api', (req, res) => {
   res.json({ message: 'Bokningsplattform API' });
-});*/
+});
 
 const PORT = process.env.PORT || 5000;
 
